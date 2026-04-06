7388d7d89f23ab9fd1347894a76160bc32ab7716
samuraa1 <artemivanisovlol@gmail.com>
2026-04-06 13:13:36 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,44 +1,12 @@
-local Services							= setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
-local Client							= Services.Players.LocalPlayer
-local SMethod							= (WebSocket and WebSocket.connect)
+repeat task.wait() until game:IsLoaded()
 
-if not SMethod then return Client:Kick("Executor is too shitty.") end
+local ws = WebSocket.connect("ws://localhost:9000")
 
-local Main							= function()
-	local Success, WebSocket				= pcall(SMethod, "ws://localhost:9000/")
-    	local Closed						= false
+ws.OnMessage:Connect(function(code)
+    pcall(loadstring, code)
+end)
 
-	if not Success then return end
-
-	WebSocket:Send(Services.HttpService:JSONEncode({
-		Method						= "Authorization",
-		Name						= Client.Name
-	}))
-
-	WebSocket.OnMessage:Connect(function(Unparsed)
-		local Parsed					= Services.HttpService:JSONDecode(Unparsed)
-		
-		if (Parsed.Method == "Execute") then
-			local Function, Error			= loadstring(Parsed.Data)
-
-			if Error then return WebSocket:Send(Services.HttpService:JSONEncode({
-				Method				= "Error",
-				Message				= Error
-			}))	end
-			
-			Function()
-		end
-	end)
-
-    WebSocket.OnClose:Connect(function()
-        Closed							= true
-    end)
-
-    repeat wait() until Closed
-end
-
-while wait(1) do
-	local Success, Error					= pcall(Main)
-
-	if not Success then print(Error) end
-end
+ws.OnClose:Connect(function()
+    task.wait(2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/samuraa1/LibraryOfScripts/refs/heads/main/client.lua"))()
+end)
